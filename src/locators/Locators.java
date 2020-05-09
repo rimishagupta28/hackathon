854ff6bff4e623e9eccc138edf4d5e1cd92a29fc
prateek2428 <65072144+prateek2428@users.noreply.github.com>
2020-05-09 17:54:46 +0530

    Update Locators.java

--- src/locators/Locators.java
+++ src/locators/Locators.java
@@ -2,13 +2,13 @@ package locators;
 import org.openqa.selenium.*;
 import org.openqa.selenium.JavascriptExecutor;
 
-public class Locators {
-  driver.findElement(By.xpath("//*[@id='header-search-field']")).sendKeys("Web Development",Keys.RETURN);
+public class Locators extends Browser {
+	public static WebElement searchtTextBox() {    //returns search box webElement
+			WebElement input = driver.findElement(By.xpath("//*[@id='header-search-field']"));
+			return input;	
+     }
+	 public static WebElement selectCourse() { //returns submit webElement
+		 WebElement select =driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[1]/div[2]/div[3]/div/div/div/ul/li[1]/a/span"));
+		 return select;
+	 }
 	
-	driver.findElement(By.xpath("/html/body/div[2]/div[2]/div[1]/div[2]/div[3]/div/div/div/ul/li[1]/a/span")).click();                                                                                                                               
-	Thread.sleep(2000);
-	JavascriptExecutor js = (JavascriptExecutor) driver;
-	 js.executeScript("window.scrollBy(0,400)");
-	driver.findElement(By.xpath("//body/div/div/div/div/div/div/div/div[1]/button[1]")).click();
-
-}
